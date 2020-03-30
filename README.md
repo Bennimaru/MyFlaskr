0b6afe79b2c8d4d3d5f2b8f1c3bd25aa848e4a97
V V <bennywhyu@gmail.com>
2020-03-29 22:10:06 -0400

    updated readme and fixed auth typos

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ TL;DR This project is a basic blog application built using Flask.
 Alright, another python project to continue leveling up in both python and the micro framework Flask. In my previous python project, I implemented routing, created a basic template, and extended the template along with corresponding routes. I think the deliverables for this new project is the next step up to becoming more proficient in developing with python. For this project, I will be following along with the tutorial provided by the official Flask documentation which one can access over at https://flask.palletsprojects.com/en/1.1.x/tutorial/. The end product will be a simple Tumblr clone. Users will be able to register, log in, and perform basic CRUD on their blogs. Small personal design tweaks will be implemented here and there. Any major feature changes different from the tutorial will be noted as such.
 
 First goal: Project setup
+Second goal: Database setup
+Third goal: Authorization blueprint
+Fourth goal: Create templates for render template function
+Fifth goal: Add basic styling
+Sixth goal: Blog blueprint
+Seventh goal: Making the project installable elsewhere
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -19,18 +19,18 @@ def register():
         error = None
 
         if not username:
-            error = 'Username is required'
+            error = 'Username is required.'
         elif not password:
-            error = 'Password is required'
+            error = 'Password is required.'
         elif db.execute(
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
-            error = 'User {} is already taken'.format(username)
+            error = 'User {} is already registered.'.format(username)
 
         if error is None:
             db.execute(
-                'INSERT INTO user(username,password) VALUES(?,?)', (username,
-                                                                    generate_password_hash(password))
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
             )
             db.commit()
             return redirect(url_for('auth.login'))
@@ -58,7 +58,7 @@ def login():
 
         if error is None:
             session.clear()
-            session['user.id'] = user['id']
+            session['user_id'] = user['id']
             return redirect(url_for('index'))
 
         flash(error)
@@ -74,7 +74,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM user WHERE username id=?', (user_id,)
+            'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
 
